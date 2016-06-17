fe208303e86bfb1d85a2796d9774f647ba984b75
AhmedFathiShaban <ahmed.1331@yahoo.com>
2016-06-17 18:55:12 +0200

    added input start to adder_subtractor_with_control . I mistakely put a wrong version the last time

--- real-design/real_design_work_space/Real_Design/src/adder_subtractor_with_control.v
+++ real-design/real_design_work_space/Real_Design/src/adder_subtractor_with_control.v
@@ -22,11 +22,12 @@
 //{{ Section below this comment is automatically maintained
 //   and may be overwritten
 //{module {adder_subtractor}}
-module adder_subtractor_with_control (A, B,result,op,clk,ce,outsider15,controlled_adder_output);
+module adder_subtractor_with_control (A, B,result,op,clk,ce,outsider15,controlled_adder_output,start);
 //}} End of automatically maintained section
 
 // -- Enter your statements here -- //
-input A,B;
+input A,B;	
+input start;
 
 wire[31:0] A,B;	  
 wire [31:0]A_no_neg_zero,B_no_neg_zero;

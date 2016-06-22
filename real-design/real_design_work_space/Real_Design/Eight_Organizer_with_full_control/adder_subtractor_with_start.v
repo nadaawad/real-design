6114dd8eb161502f6e1d47eb37a14600f3d138a1
AhmedFathiShaban <ahmed.1331@yahoo.com>
2016-06-22 15:48:58 +0200

    now Eight organizer with control row depends on adders' finish signals correctly

--- real-design/real_design_work_space/Real_Design/Eight_Organizer_with_full_control/adder_subtractor_with_start.v
+++ real-design/real_design_work_space/Real_Design/Eight_Organizer_with_full_control/adder_subtractor_with_start.v
@@ -19,27 +19,27 @@
 //-----------------------------------------------------------------------------
 `timescale 1 ns / 1 ps
 
-//{{ Section below this comment is automatically maintained
-//   and may be overwritten
-//{module {adder_subtractor}}
-module adder_subtractor_with_start (A, B,result,op,clk,ce,outsider15,controlled_adder_output,start);
-//}} End of automatically maintained section
 
-// -- Enter your statements here -- //
-input A,B;	
-input start;
+module adder_subtractor_with_start (start,A, B,result,op,clk,ce,finish);
 
-wire[31:0] A,B;	  
+input A,B,start;
+output reg finish=0;
+
+wire[31:0] A,B;	 
+
 wire [31:0]A_no_neg_zero,B_no_neg_zero;
 assign A_no_neg_zero =(A[30:0]==31'b0)?32'b0:A;
 assign B_no_neg_zero =(B[30:0]==31'b0)?32'b0:B;
 
-input clk ,op,ce;
-output result;	 
 
-input wire outsider15;
-output wire [31:0] controlled_adder_output;	
-reg [31:0] previous_value;
+
+
+
+input clk ,op,ce;
+output result;
+reg pip1=0;
+reg pip2=0;
+
 
 wire [31:0] result;
 wire[33:0] Radd,Rsub;
@@ -54,14 +54,23 @@ assign modified_result= op? Rsub:Radd;
 
 InputIEEE_8_23_to_8_23 A3 (.clk(clk),.rst(1'b0),.X(modified_result), .R( result));
 
-
-assign controlled_adder_output = outsider15?result:previous_value;
-always@(posedge clk)
-	begin 
-		if(outsider15==1)
-			previous_value <= result;
-		else if (!start )
-			previous_value <= 32'b0;		
-	end	
+always @(posedge clk)
+	begin
+	
+		if(op==0&&A_no_neg_zero[31]== B_no_neg_zero[31])
+			begin
+			finish<=start;
+			
+			end
+			
+		else
+			begin
+			pip1<=start;
+			pip2<=pip1;
+			finish<=pip2;
+			end
+			
+			
+	end
 
 endmodule
--- real-design/real_design_work_space/Real_Design/src/8_organizer_with_control_row.v
+++ real-design/real_design_work_space/Real_Design/src/8_organizer_with_control_row.v
@@ -1,5 +1,5 @@
 
-module Eight_Organizer_with_control_row(clk,adder_row_input,start,adder_output,outsider4,outsider15,outsider11);
+module Eight_Organizer_with_control_row(clk,adder_row_input,start,adder_output,outsider4,final_adder_finish_dash,ExE_finish);
 parameter NI = 8;
 input clk ;
 
@@ -16,28 +16,23 @@ output wire [31:0] adder_output ;
 
 
 input wire outsider4;
-reg outsider5=0;
-reg outsider6=0;
-reg outsider7=0;
-reg outsider8=0;
-reg outsider9=0; 
-reg outsider10=0; 
-output reg outsider11=0; 
-reg outsider12=0; 
-reg outsider13=0;  
-reg outsider14=0;
 
-output reg outsider15=0; 
-
+output wire final_adder_finish_dash;   //  // NOTE :: THIS IS EQUIVALENT TO THE FORMER  OUTSIDER15
 reg first_time = 1;
 
 
 reg [31:0] second_pre_last_output;
-wire [31:0] controlled_adder_output;
+wire [31:0] controlled_adder_output; 
+
+output wire ExE_finish;  // NOTE :: THIS IS EQUIVALENT TO THE FORMER  OUTSIDER11 
+
+reg ExE_finish_dash_dash=0;
+wire final_adder_finish; 
+
 
 TwoxOne_mux m1 (controlled_adder_output,mux_one,mux_output,mux_select);
-EightxEight_Adder A1 (clk,adder_row_input,pre_last_output);
-adder_subtractor_with_control final_adder (mux_output,second_pre_last_output , adder_output, 0,clk,1,outsider15,controlled_adder_output,start);
+EightxEight_Adder_with_start A1 (outsider4,clk,adder_row_input,pre_last_output,ExE_finish,ExE_finish_dash);
+adder_subtractor_with_control_with_start final_adder (ExE_finish_dash_dash,mux_output,second_pre_last_output , adder_output, 0,clk,1,controlled_adder_output,start,final_adder_finish,final_adder_finish_dash);
 
 
 always @(posedge clk)
@@ -49,11 +44,11 @@ begin
 		end
 	else 
 		begin
-			if( outsider12 ==1 && !first_time)
+			if( ExE_finish_dash ==1 && !first_time)
 				begin
 					mux_select <= 0;  
 				end	
-			else if (outsider12 ==1)
+			else if (ExE_finish_dash ==1)
 				begin	
 					first_time<=0;
 				end	
@@ -63,24 +58,12 @@ begin
 end	 
 
 
-always @(posedge clk)
-	begin 
-	outsider5<=outsider4;
-	outsider6<=outsider5;
-	outsider7<=outsider6;
-	outsider8<=outsider7;
-	outsider9<=outsider8; 
-	outsider10<=outsider9;
-	outsider11<=outsider10;
-	outsider12<=outsider11;
-	outsider13<=outsider12;
-	outsider14<=outsider13;	   
-	outsider15<=outsider14;
-	end		 
+	 
 	
 	always @(posedge clk)
 		begin
-			second_pre_last_output<= pre_last_output;	
+			second_pre_last_output<= pre_last_output;
+			ExE_finish_dash_dash<= ExE_finish_dash;
 		end	
 
 
